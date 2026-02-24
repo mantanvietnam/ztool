26dec6d43ed019fd91979658114a904906aed8e9
mantanvietnam <71007853+mantanvietnam@users.noreply.github.com>
2026-02-24 22:45:27 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -22,7 +22,7 @@ const nextConfig = {
           {
             key: 'Content-Security-Policy',
             // Dòng dưới đây là nơi bạn cấu hình
-            value: "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://ztool.phoenixtech.vn https://apis.ztool.ai.vn https://www.google-analytics.com https://www.googletagmanager.com https://sp.zalo.me https://zjs.zalo.me; object-src 'none';", 
+            value: "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://ztool.phoenixtech.vn https://ztool.ai.vn https://apis.ztool.ai.vn https://www.google-analytics.com https://www.googletagmanager.com https://sp.zalo.me https://zjs.zalo.me https://oauth.zaloapp.com https://static.cloudflareinsights.com; object-src 'none';",
           },
         ],
       },
